6c8e8a3127c2635428bd4887a91c723113baa4b0
Brodie <88861642+brodiep21@users.noreply.github.com>
2022-06-12 13:23:55 -0400

    Update blocks_test.go

--- problem/blocks_test.go
+++ problem/blocks_test.go
@@ -7,9 +7,20 @@ import (
 )
 
 func TestBocks(t *testing.T) {
-	s := "abbabbaaa"
-	minNumLetters := blocks.FindMinNumberOfAdditionalLettersForBlocksToBeEqualLengths(s)
-	if minNumLetters != 6 {
-		t.Errorf("got %d, wanted 6", minNumLetters)
-	}
+	t.Run("scenario 1", func(t *testing.T) {
+		s := "abbabbaaa"
+		minNumLetters := blocks.FindMinNumberOfAdditionalLettersForBlocksToBeEqualLengths(s)
+		if minNumLetters != 6 {
+			t.Errorf("got %d, wanted 6", minNumLetters)
+		}
+	})
+
+	t.Run("scenario 2", func(t *testing.T) {
+		s := "abbabbaaabbbaabbbaaaa"
+		minNumLetters := blocks.FindMinNumberOfAdditionalLettersForBlocksToBeEqualLengths(s)
+		if minNumLetters != 16 {
+			t.Errorf("got %d, wanted 16", minNumLetters)
+		}
+	})
+
 }
