c9dd842b3db7f04b3cf999dbe12b5463d2acff3e
Brodie <88861642+brodiep21@users.noreply.github.com>
2022-06-12 12:31:48 -0400

    Update private_test.go

--- problem/private_test.go
+++ problem/private_test.go
@@ -4,22 +4,23 @@ import "testing"
 
 func TestFindBlocks(t *testing.T) {
 	tests := []struct {
-		input  string
-		block  int
-		output string
+		input         string
+		block         int
+		startingIndex int
+		endingIndex   int
 	}{
-		{"ab", 0, "a"},
-		{"aab", 0, "aa"},
-		{"aaaaaab", 0, "aaaaaa"},
-		{"ab", 1, "b"},
-		{"aab", 1, "b"},
-		{"aabb", 1, "bb"},
+		{"ab", 0, 0, 1},
+		{"aab", 0, 0, 2},
+		{"aaaaaab", 0, 0, 6},
+		{"ab", 1, 1, 2},
+		{"aab", 1, 2, 3},
+		{"aabb", 1, 2, 4},
 	}
 
 	for _, tc := range tests {
-		block := findBlock(tc.input, tc.block)
-		if block != tc.output {
-			t.Errorf("got %q, want %q", block, tc.output)
+		startingIndex, endingIndex := findBlock(tc.input, tc.block)
+		if startingIndex != tc.startingIndex && endingIndex != tc.endingIndex {
+			t.Errorf("got %d, %d, want %d, %d", startingIndex, endingIndex, tc.startingIndex, tc.endingIndex)
 		}
 	}
 }
